fbdd3172dc55c33f5a9249bbd4855cf7aaaef89f
Masayuki Sunahara <tamanishi@gmail.com>
2020-04-15 18:14:30 +0900

    update episodes

--- update.sh
+++ update.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# . .apienv
-# curl ${API_ENDPOINT} --header "x-api-key:${API_KEY}" >| ./episodes.json 
+. .env
 
-/home/pi/src/rebuild_feed_to_json/target/release/rebuild_feed_to_json >| ./episodes.json
+${GENERATE_EPISODES_JSON_CMD} >| ./episodes.json
 cat ./template1 >| ./index.js
 cat ./episodes.json >> ./index.js
 cat ./template2 >> ./index.js
